0b3c0f1cfd6ee4339456049b8392282f3c23ae52
Anjul Singh <118563318+npmanjul@users.noreply.github.com>
2024-08-09 17:56:58 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,31 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack(primitive) && Heap(Non-primitive)
+
+let myName="Anjul";
+let anotherName=myName;
+anotherName="Anjul Singh";
+
+console.log(myName);
+console.log(anotherName);
+
+
+let user={
+    email:"anjulsingh462@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo=user;
+userTwo.email="anjulsingh000@gmaul.com"
+
+console.log(user)   //anjulsingh000@gmaul.com
+console.log(userTwo)  //anjulsingh000@gmaul.com
+
+
+//https://firebasestorage.googleapis.com/v0/b/fir-53dbe.appspot.com/o/Screenshot%20(4095).png?alt=media&token=cf4171e8-616e-40ff-945c-05d9f7984ba3
