4f862b96cebc1c7920f5443ae131e75dff3141e6
Anjul Singh <118563318+npmanjul@users.noreply.github.com>
2024-08-11 22:28:14 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Jaipur"    //again declare with var , not with const and let
 let accountState;
 
 // accountId = 2 // not allowed
